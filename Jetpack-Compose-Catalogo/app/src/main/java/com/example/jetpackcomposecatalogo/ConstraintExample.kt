0aca84bae67bdc0f9dbae6f06e967e34e48b9f7d
Veselin Velkov <vesselin60@gmail.com>
2023-02-21 17:15:08 +0200

    14. [Ejercicio2] ConstraintLayout básico, 15. (SOLUCIÓN) [Ejercicio 2] ConstraintLayout básico

--- Jetpack-Compose-Catalogo/app/src/main/java/com/example/jetpackcomposecatalogo/ConstraintExample.kt
+++ Jetpack-Compose-Catalogo/app/src/main/java/com/example/jetpackcomposecatalogo/ConstraintExample.kt
@@ -22,25 +22,48 @@ fun ConstraintExample1() {
 
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
 
-        val (boxRed, boxBlue, boxYellow, boxMagenta) = createRefs()
+        val (boxRed, boxBlue, boxYellow, boxMagenta, boxGreen) = createRefs()
 
-        Box(modifier = Modifier.size(125.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom)
-        })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
 
-        Box(modifier = Modifier.size(125.dp).background(Color.Blue))
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                top.linkTo(boxRed.bottom)
+                start.linkTo(boxRed.end)
+            })
 
-        Box(modifier = Modifier.size(125.dp).background(Color.Yellow).constrainAs(boxYellow){
-            bottom.linkTo(boxRed.top)
-            end.linkTo(boxRed.start)
-        })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
 
-        Box(modifier = Modifier.size(125.dp).background(Color.Magenta).constrainAs(boxMagenta){
-            bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.end)
-        })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
     }
 }
