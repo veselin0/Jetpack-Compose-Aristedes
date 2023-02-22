030c9376c7859023dce1d1e6e6829fab2a5d64ba
Veselin Velkov <vesselin60@gmail.com>
2023-02-22 09:34:43 +0200

    20. RememberSaveable

--- Jetpack-Compose-Catalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ Jetpack-Compose-Catalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyStateExample() {
 
-    val counter = remember { mutableStateOf(0) }
+    val counter = rememberSaveable { mutableStateOf(0) }
 
     Column(
         Modifier.fillMaxSize(),
