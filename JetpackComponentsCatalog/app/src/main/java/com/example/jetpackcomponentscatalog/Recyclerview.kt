089409f38143c4cc38012935cb919b3480be29bf
Veselin Velkov <vesselin60@gmail.com>
2023-03-16 15:05:16 +0200

    69. Controlar el desplazamiento

--- JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Recyclerview.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Recyclerview.kt
@@ -11,18 +11,25 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomponentscatalog.model.Superhero
+import kotlinx.coroutines.launch
 
 @Composable
 fun SimpleRecyclerView() {
@@ -47,6 +54,39 @@ fun SuperHeroView() {
     }
 }
 
+@Composable
+fun SuperHeroWithSpecialControlsView() {
+    val context = LocalContext.current
+    val rvState = rememberLazyListState()
+    val coroutinesScope = rememberCoroutineScope()
+    Column {
+        LazyColumn(
+            state = rvState, verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f)
+        ) {
+            items(getSuperheroes()) { superhero ->
+                ItemHero(superhero = superhero)
+                { Toast.makeText(context, it.superheroName, Toast.LENGTH_SHORT).show() }
+            }
+        }
+
+
+//        rvState.firstVisibleItemScrollOffset
+
+        Button(
+            onClick = {
+                coroutinesScope.launch { rvState.animateScrollToItem(0) }
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        ) {
+            Text(text = "Soy un boton coolo!")
+        }
+
+    }
+}
+
 
 @Composable
 fun SuperHeroGridView() {
@@ -65,7 +105,7 @@ fun ItemHero(superhero: Superhero, onItemSelected: (Superhero) -> Unit) {
     Card(
         border = BorderStroke(2.dp, Color.Red),
         modifier = Modifier
-            .width(200.dp)
+            .fillMaxWidth()
             .clickable { onItemSelected(superhero) }) {
         Column {
             Image(
