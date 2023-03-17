e1d7d4474cf9a97b16d0090b8748d7f963ebe3d7
Veselin Velkov <vesselin60@gmail.com>
2023-03-17 11:34:39 +0200

    71. Scaffold

---
+++ Controlareldesplazamiento/app/src/main/java/com/example/controlareldesplazamiento/Scaffold.kt
@@ -0,0 +1,19 @@
+package com.example.controlareldesplazamiento
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScaffoldExample() {
+    Scaffold() {
+
+    }
+}
