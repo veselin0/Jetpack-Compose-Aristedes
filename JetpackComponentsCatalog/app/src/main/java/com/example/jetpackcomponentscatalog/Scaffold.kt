665e907cb333065f849d62926bde525153952a37
Veselin Velkov <vesselin60@gmail.com>
2023-03-21 09:52:27 +0200

    73. Snackbar

--- JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
@@ -12,41 +12,52 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScaffoldExample() {
-    Scaffold(topBar = { MyTopAppBar() }) { contentPadding ->
+
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(topBar = {
+        MyTopAppBar {
+            coroutineScope.launch {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    "You clicked $it"
+                )
+            }
+        }
+    }, scaffoldState = scaffoldState) { contentPadding ->
         Box(Modifier.padding(contentPadding))
     }
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "My First Toolbar!") },
         backgroundColor = Color.Red,
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Back!") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Search!") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "search")
             }
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Close!") }) {
                 Icon(imageVector = Icons.Filled.Close, contentDescription = "close")
             }
         }
-
     )
-
-
 }
