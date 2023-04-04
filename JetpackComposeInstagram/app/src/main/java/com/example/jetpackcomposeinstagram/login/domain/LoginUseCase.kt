eaa03b10da44d0c7ef691fd7ace170a57bcf2dc3
Veselin Velkov <vesselin60@gmail.com>
2023-04-04 09:21:43 +0300

    Some minor changes

---
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/domain/LoginUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.jetpackcomposeinstagram.login.domain
+
+class LoginUseCase {
+    private val repository = LoginRepository()
+
+    suspend operator fun invoke(user:String, password:String):Boolean{
+        return repository.doLogin(user, password)
+    }
+}
