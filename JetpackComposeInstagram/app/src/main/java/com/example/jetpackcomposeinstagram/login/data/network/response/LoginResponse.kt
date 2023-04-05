e1aec5bb7cd1408ce7f0e4f8bec3fe07601ab88f
Veselin <vesselin60@gmail.com>
2023-04-05 08:42:36 +0300

    JetpackComposeInstagram/app deleted

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/data/network/response/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.cursokotlin.jetpackcomposeinstagram.login.data.network.response
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResponse(@SerializedName("success") val success:Boolean)
