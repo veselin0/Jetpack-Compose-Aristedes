5622756fd90dd6b9a0a0f62e2dd7b661551f5f43
Veselin <vesselin60@gmail.com>
2023-04-07 08:53:26 +0300

    93. Inyectando Activity y ViewModel

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposeinstagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +14,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposeinstagram.login.ui.LoginScreen
 import com.example.jetpackcomposeinstagram.login.ui.LoginViewModel
 import com.example.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.log
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LoginScreen(LoginViewModel())
+                    LoginScreen(loginViewModel)
                 }
             }
         }
--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
@@ -7,11 +7,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcomposeinstagram.login.domain.LoginUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
-
-    val loginUseCase = LoginUseCase()
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
+    
 
     private val _email = MutableLiveData<String>()
     val email: LiveData<String> = _email
