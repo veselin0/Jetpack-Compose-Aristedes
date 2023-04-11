901b8f821218326e90e42fd9b1b9eecfb1c5429e
Veselin <vesselin60@gmail.com>
2023-04-11 11:58:54 +0300

    Dagger Hilt not working

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/data/network/LoginService.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/data/network/LoginService.kt
@@ -1,15 +1,16 @@
 package com.example.jetpackcomposeinstagram.login.data.network
 
-import com.example.jetpackcomposeinstagram.core.network.RetrofitHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import javax.inject.Inject
 
-class LoginService {
-    private val retrofit = RetrofitHelper.getRetrofit()
+class LoginService @Inject constructor(private val loginClient: LoginClient) {
+//    private val retrofit = RetrofitHelper.getRetrofit()
 
     suspend fun doLogin(user: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
-            val response = retrofit.create(LoginClient::class.java).doLogin()
+            val response = loginClient.doLogin()
             response.body()?.success ?: false
         }
     }
