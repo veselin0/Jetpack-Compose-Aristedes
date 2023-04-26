d2b88ebdc5ff68384b9d085e4c0f53fc18253ec7
Veselin Velkov <vesselin60@gmail.com>
2023-04-26 10:05:20 +0300

    118. Finders

--- ComposeTesting/app/src/androidTest/java/com/example/composetesting/components/GochoComponentTest.kt
+++ ComposeTesting/app/src/androidTest/java/com/example/composetesting/components/GochoComponentTest.kt
@@ -1,5 +1,6 @@
 package com.example.composetesting.components
 
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
 import org.junit.Test
@@ -13,5 +14,15 @@ class GochoComponentTest {
         composeTestRule.setContent {
             GochoComponent()
         }
+
+//        FINDER:
+//        composeTestRule.onNodeWithText("GOCHO")
+        composeTestRule.onNodeWithText("gocho", ignoreCase = true)
+        composeTestRule.onNodeWithTag("component1")
+        composeTestRule.onNodeWithContentDescription("superImage")
+
+        composeTestRule.onAllNodesWithText("g")
+        composeTestRule.onAllNodesWithTag("component1")
+        composeTestRule.onAllNodesWithContentDescription("visualIcon")
     }
 }
--- ComposeTesting/app/src/main/java/com/example/composetesting/components/GochoComponent.kt
+++ ComposeTesting/app/src/main/java/com/example/composetesting/components/GochoComponent.kt
@@ -1,8 +1,22 @@
 package com.example.composetesting.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun GochoComponent() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "GOCHO", Modifier.testTag("component1"))
+        Text(text = "GOCHO", Modifier.testTag("component2"))
+        
+        Image(Icons.Default.Add, contentDescription = "superImage")
 
+    }
 }
